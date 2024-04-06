4df94047a84f8a550f6db4796fd5ca2dbd45ddd7
truekendor <sadqwerty000@gmail.com>
2024-04-06 11:20:27 +0300

    move function to sidebar component

--- src/sidebar.ts
+++ src/sidebar.ts
@@ -1,9 +1,14 @@
 export const detectionSidebar: HTMLDivElement =
   document.querySelector(".detection-sidebar")!;
 
+export function expandSidebar() {
+  detectionSidebar.classList.remove("shrink");
+}
+
 const sidebarExpandBtn: HTMLButtonElement = document.querySelector(
   "button.sidebar-expand-btn"
 )!;
+
 sidebarExpandBtn.addEventListener("click", () => {
   detectionSidebar.classList.toggle("shrink");
 });
